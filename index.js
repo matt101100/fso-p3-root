95738387bd9f073784af12cc121271a6b5e95e76
Matthew Lazar <matthewlazar@Matthews-Air.modem>
2025-01-06 19:21:33 +1100

    added frontend production build (3.9)

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 let persons = [
     { 
@@ -110,7 +113,7 @@ const unknownEndpoint = (request, response) => {
   
   app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
